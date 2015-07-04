649a8857368fb4d895637eea01d3a36650346bf2
koberlag <koberlag@gmail.com>
2015-07-04 11:18:21 -0500

    completed animation and audio

--- js/app.js
+++ js/app.js
@@ -5,40 +5,40 @@ $(document).ready(function()
 		ryuStill = $(".ryu-still"),
 		ryuCool = $(".ryu-cool"),
 		ryuThrowing = $(".ryu-throwing"),
-		hadouken = $(".hadouken"),
-		audioElement = document.createElement('audio');
-        
-        audioElement.setAttribute('src', 'sound/hadouken.mp3');
-        audioElement.setAttribute('autoplay', 'autoplay');
+		hadouken = $(".hadouken");
 
 
 	ryuContainer.hover(function ()
 	{
-		ryuReady.toggle();
-		ryuStill.toggle();
+		ryuReady.show();
+		ryuStill.hide();
 	},	
 	function ()
 	{
-		ryuReady.toggle();
-		ryuStill.toggle();
+		ryuReady.hide();
+		ryuStill.show();
 	}).mousedown(function()
 	{
-		// play hadouken sound
-		audioElement.play();
-
-		ryuReady.toggle();
-		ryuThrowing.toggle();
-		hadouken.toggle();
-		//animate hadouken to the right of the screen
+		playHadouken();
+		ryuReady.hide();
+		ryuThrowing.show();
+		hadouken.finish().show().animate(
+		  {'left': '1020px'},
+		  500,
+		  function() {
+		    $(this).hide();
+		    $(this).css('left', '-212px');
+		  }
+		);
     	
 	}).mouseup(function()
 	{
-		console.log('mouseup');
-		ryuReady.toggle();
-		ryuThrowing.toggle();
-		hadouken.toggle();
+		ryuReady.show();
+		ryuThrowing.hide();
 	});
-
-	
-
 })
+	function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
